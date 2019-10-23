f434fa08f8ab65646feed111517e5c20bcf4cf73
RileyJJ794 <hoffman@Hoffmans-MacBook-Pro.local>
2019-10-23 13:31:14 -0500

    updated java script

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ var display = document.getElementById('screen');
 var operator = document.getElementsByClassName('operator');
 var clearBtn = document.getElementById('AC');
 var backBtn = document.getElementById('back');
+var decimalBtn = document.getElementById('decimal');
 
 var displayValue = 0;
+var pendingValue;
+var evalStringArray = [];
 
 const updateDisplayValue = (clickObj) => {
-<<<<<<< HEAD
     const btnText = clickObj.target.innerText;
-=======
-    const btnText = clickObj.target.innerHTML;
->>>>>>> f645f1ba61206776a6f49a9fabbffdd5cdd20eeb
     console.log(btnText);
 
     if (displayValue === 0) {
@@ -22,36 +21,92 @@ const updateDisplayValue = (clickObj) => {
     display.innerHTML = displayValue;
 };
 
+const performOperation = (clickObj) => {
+    var operatorBtnValue = clickObj.target.innerText;
+    console.log(operatorBtnValue + 'pressed')
+
+    switch (operatorBtnValue) {
+        case '+':
+            pendingValue = displayValue;
+            displayValue = 0;
+            display.innerHTML = displayValue;
+            evalStringArray.push(pendingValue);
+            evalStringArray.push('+');
+            
+            break;
+
+        case '-':
+            pendingValue = displayValue;
+            displayValue = 0;
+            display.innerHTML = displayValue;
+            evalStringArray.push(pendingValue);
+            evalStringArray.push('-');
+                        
+            break;
+
+        case '×':
+            pendingValue = displayValue;
+            displayValue = 0;
+            display.innerHTML = displayValue;
+            evalStringArray.push(pendingValue);
+            evalStringArray.push('*');
+                    
+            break;
+
+        case '÷':
+            pendingValue = displayValue;
+            displayValue = 0;
+            display.innerHTML = displayValue;
+            evalStringArray.push(pendingValue);
+            evalStringArray.push('/');
+                            
+            break;
+        
+        case '=':
+            evalStringArray.push(displayValue);
+            var evaluation = eval(evalStringArray.join(' '));
+            displayValue = evaluation + '';
+            display.innerHTML = displayValue;
+            evalStringArray = [];
+        
+            break;
+        default:
+            break;
+    }
+}
+
+
 clearBtn.onclick = () => {
     displayValue = 0;
+    pendingValue = '';
+    evalStringArray = [];
     display.innerHTML = displayValue;
 };
 
 backBtn.onclick = () => {
     var lengthofDisplayVal = displayValue.length;
-    console.log(displayValue.length);
-<<<<<<< HEAD
     displayValue = displayValue.slice(0, lengthofDisplayVal - 1);
-=======
-    displayValue = displayValue.slice(0, lengthofDisplayVal - 31);
->>>>>>> f645f1ba61206776a6f49a9fabbffdd5cdd20eeb
-    console.log('bakc button pressed');
+
     if (displayValue === '') {
         displayValue = 0;
-        console.log('the if statment');
     };
 
-    console.log(displayValue.length);
+    display.innerHTML = displayValue;
+};
 
+decimalBtn.onclick = () => {
+    if(!displayValue.includes('.')){
+        displayValue += '.';
+    };
     display.innerHTML = displayValue;
 };
 
 
 
-for (var i = 0; i < numberBtn.length; i++) {
+for (let i = 0; i < numberBtn.length; i++) {
     numberBtn[i].addEventListener('click', updateDisplayValue, false);
 };
 
-for (var i = 0; i < operator.length; i++) {
+for (let i = 0; i < operator.length; i++) {
     operator[i].addEventListener('click', performOperation, false);
 };
